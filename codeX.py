4ac3997124b941f8a76511ad451f7e73f3bc3d90
Nícolas dos Santos Carvalho <ra128660@uem.br>
2023-07-10 00:21:20 -0300

    refatoração e readequação no código

--- codeX.py
+++ codeX.py
@@ -2,28 +2,31 @@
 # Nícolas dos Santos Carvalho
 # UEM 2022
 
+from enum import Enum
 from datetime import date
 import sys
 import os
 import re
 
+class IndentationType(Enum): SPACE = 1; TAB = 2;
+
 ################################################################################
-def orderFiles(l):
+def sortFiles(l: list[str]):
     """
     Ordene os arquivos e os exercicios para que ordem devem ficar
     por padrão ordena pela data e número do exercicio
     """
 
-    # fileNumbers = list(map(splitFileName, l))
+    fileNumbers = list(map(splitFileName, l))
 
-    # newL = []
-    # for f in fileNumbers:
-    #     newL = sortingFilesList(newL, f)
-    # newL = list(map(lambda f: f["name"], newL))
+    newL = []
+    for f in fileNumbers:
+        newL = sortingFilesList(newL, f)
+    newL = list(map(lambda f: f["name"], newL))
 
-    print(l)
+    print(newL)
 
-    return l
+    return newL
 
 def mapLines(l):
     """
@@ -41,16 +44,20 @@ def filterLines(l):
 
     return True
 
-# fileregex = r".*_\d{2}\..+$"
-fileregex = r".*\..+$"
+fileregex = r"\d{8,}_\d{2}\..+$"
 numberegex = r"(?<=_)\d{2}(?=\..+$)"
 dateregex = r"\d{8,}(?=_)"
 
 opts = {
     "strict": True,
     "absolute_numeration": True,
+
+    "indentation_type": IndentationType.SPACE,
+    "indentation_size": 4,
+    "block_scoping": True,
+
     "name": "Nícolas dos Santos Carvalho",
-    "ra": "128660"
+    "ra": "128660",
 }
 ################################################################################
 
@@ -80,7 +87,7 @@ def main(argv):
         fileNames
     ))
 
-    fileNames = orderFiles(fileNames)
+    fileNames = sortFiles(fileNames)
 
     for f in fileNames:
         # message(os.path.join(folderPath, f), 0)
@@ -88,7 +95,7 @@ def main(argv):
         fileNumber = reg.group() if reg != None else -1;
         interp = Interpreter()
         interp.interpretFiles(os.path.join(folderPath, f))
-        setupParts(interp.partitions, fileNumber)
+        setupParts(interp.partitions, int(fileNumber))
 
     writeFile(os.path.join(folderPath), partsCombined)
     return 0
@@ -98,10 +105,10 @@ def writeFile(filePath, parts):
 
     with open(os.path.join(filePath, "./out.c"), "w") as file, open("./.templates/trabalho.c") as template:
         templ = template.read()                                 \
-                        .replace("$NOME", f"{opts.name}")      \
-                        .replace("$RA", f"{opts.ra}")      \
-                        .replace("$DD", f"{today.day:02}")      \
-                        .replace("$MM", f"{today.month:02}")    \
+                        .replace("$NOME", f"{opts['name']}")    \
+                        .replace("$RA",   f"{opts['ra']}")      \
+                        .replace("$DD",   f"{today.day:02}")    \
+                        .replace("$MM",   f"{today.month:02}")  \
                         .replace("$AAAA", f"{today.year:04}")   \
                         .replace("$INCL", parts["incl"])        \
                         .replace("$EXPL", parts["expl-all"])    \
@@ -123,34 +130,45 @@ def writeFile(filePath, parts):
 def setupParts(parts, exNumber):
     global relativeExNumber
     body = ""
-    relativeExNumber += 1
-    # print(exNumber, parts)
-    partsCombined["incl"] += parts["incl"]
 
-    number = exNumber if opts['absolute_numeration'] else relativeExNumber;
+    if parts["main"] != "":
+        relativeIndentation = 0
+        relativeExNumber += 1
+        partsCombined["incl"] += parts["incl"]
 
-    if number >= 0:
-       body = (
-           f"    // {number if number >= 0 else ''}\n" +
-           "    {\n"
-        );
+        number = exNumber if opts['absolute_numeration'] else relativeExNumber;
 
-    body += (
-        increaseIndentation(parts["expl"], 4) + "\n" +
-        increaseIndentation(parts["main"], 0)
-    ) if parts["main"] != "" else ""
+        if number >= 0:
+            body = increaseIndentation(f"\n// {number}\n", 1)
 
-    if number >= 0:
-        body += "}\n\n"
+        if opts["block_scoping"]:
+            body += increaseIndentation("{\n", 1);
+            relativeIndentation += 1
+
+        if parts["expl"] != "":
+            body += increaseIndentation(parts["expl"], relativeIndentation+1) + "\n"
+
+        body += increaseIndentation(parts["main"], relativeIndentation)
+
+        if opts["block_scoping"]:
+            body += increaseIndentation("}\n", relativeIndentation)
+            relativeIndentation -= 1
 
     partsCombined["main"] += body
-
     partsCombined["expl-all"] += parts["expl-all"]
     partsCombined["defn"] += parts["defn"]
     partsCombined["func"] += parts["func"]
 
-def increaseIndentation(str, space):
-    return (" "*space)+str.replace("\n", "\n"+(" "*space))
+def getIndentationType():
+    return (" " if opts["indentation_type"] == IndentationType.SPACE else "\t")
+
+def increaseIndentation(str: str, space: int):
+    space_number = opts["indentation_size"] * (space if opts["indentation_type"] == IndentationType.SPACE else space//4)
+    indentation = getIndentationType()*space_number
+
+    str = indentation + str.replace("\n", "\n" + indentation)
+
+    return str.rstrip(getIndentationType())
 
 def splitFileName(str):
     return {
@@ -169,7 +187,7 @@ def sortingFilesList(filesList: list[str], file):
     print(file["name"])
     for i in range(0, len(filesList)):
         strCmp = strDateCompare(filesList[i]["date"], file["date"])
-        if strCmp == 1 or (strCmp == 0 and filesList[i]["number"] > file["number"]):
+        if strCmp == -1 or (strCmp == 0 and filesList[i]["number"] < file["number"]):
             filesList.insert(i, file)
             return filesList
 
@@ -232,8 +250,8 @@ class Interpreter:
             while d["type"] != "end":
                 i += 1
                 d = self.parseData(lines, i)
-                l = d["value"]
-                body += l if l.strip() != "\r\n" else ""
+                l = d["value"].replace("\r", "")
+                body += l
                 i = d["index"]
             # print("//@start "+name+":\n"+body)
 
